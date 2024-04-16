0ae5a66676cb8729be8ea78f6a7845b9fe6e5c73
Lama-Khaled-Ismail <lamakhaledismail@gmail.com>
2024-04-16 17:13:25 +0200

    finished updateProfile.php

--- sanitize.php
+++ sanitize.php
@@ -8,4 +8,8 @@ function sanitize_input($input) {
     $input = htmlspecialchars($input);
     return $input;
 }
+
+function isValidPhoneNumber($phoneNumber) {
+    return preg_match("/^[0-9]+$/", $phoneNumber) && strlen($phoneNumber) <=11;
+}
 ?>
--- updateProfile.php
+++ updateProfile.php
@@ -1,5 +1,7 @@
 <?php
     include('session.php');
+    include('sanitize.php');
+    require_once('config.php');
     $username = $_SESSION['username'];
      //for testing purposes
     //$username='menna hussein';
@@ -8,12 +10,41 @@
     if(isset($_POST['submit']))
     {
         
-        var_dump($_POST);
+        
         if ($_SERVER["REQUEST_METHOD"] == "POST"){
-            $name=$_POST["name"];
-            $password=$_POST["password"];
-            $email=$_POST["email"];
-            $tel=$_POST["tel"];
+
+            // Validaiting and SANTIZING INPUT
+            $name = isset($_POST['name']) ? sanitize_input($_POST["name"]) : '';
+            $password = isset($_POST['password']) ? $_POST['password'] : '';
+            $email = isset($_POST['email']) ? filter_var($_POST['email'], FILTER_SANITIZE_EMAIL) : '';
+            $tel = isset($_POST['tel']) ? filter_var($_POST['tel'], FILTER_SANITIZE_NUMBER_INT) : '';
+            
+            $errors = [];
+            if (empty($name)) {
+                $errors[] = "Name is required";
+            }
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors[] = "Invalid email format";
+            }
+            if (empty($password)) {
+                $errors[] = "Password is required";
+            }
+            if(empty($email)){
+                $errors[] = "email is required";
+            }
+            if(!(isValidPhoneNumber($tel))){
+                $errors[] = "Invalid telephone number";
+            }
+
+            if (!empty($errors)) {
+                foreach ($errors as $error) {
+                    echo $error . "<br>";
+                }
+                
+                exit();
+            }
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            
             // Process image upload
             $imageData = $_POST["image"];
 
@@ -24,68 +55,119 @@
 
             $decodedPassportImageData = base64_decode($passportImageData);
 
-            $host = 'localhost';
-            $user = 'root';
-            $pass = '';
-            $db = 'flight_booking';
+            
        
-            $conn = mysqli_connect($host, $user, $pass, $db);
+            $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD,DB_NAME);
        
         if (!$conn) {
            die("Connection failed: " . mysqli_connect_error());
         }
-        $sql = "UPDATE passenger SET Name='$name', email='$email', password='$password' ,tel=$tel  WHERE Name='$username'";
         
-        $result = mysqli_query($conn, $sql);
-        if (!$result) {
-            echo 1;
-           die("Query failed: " . mysqli_error($conn));
-       }
-       $_SESSION['username'] = $name;
-       $username=$_SESSION['username'];
-       if (isset( $_POST["image"])){
+        // escape input
+        $name = mysqli_real_escape_string($conn, $name);
+        $email = mysqli_real_escape_string($conn, $email);
+        $password = mysqli_real_escape_string($conn, $password);
+        $tel = mysqli_real_escape_string($conn, $tel);
+        $username = mysqli_real_escape_string($conn, $username);
+
+
+        // PREPARED STATMENT
+        $stmt = $conn->prepare("UPDATE passenger SET Name=?, email=?, password=?, tel=? WHERE Name=?");
+
+        // Bind parameters
+        $stmt->bind_param('sssss', $name, $email, $password, $tel, $username);
+
+        $stmt->execute();
+
+        if ($stmt->errno) {
+            echo "Query failed: " . $stmt->error;
+        } 
+
+        $stmt -> close();
+
+        $_SESSION['username'] = $name;
+        $username=$_SESSION['username'];
+
+        if (isset( $_POST["image"])){
             $data = $_POST["image"];
             $decodedData = base64_decode($data);
 
-    // Escape and quote the binary data
-        $escapedData = $conn->real_escape_string($decodedData);
+            if($decodedData !== false){
 
-    // Update user photo in the database directly using variable interpolation
-        $sql = "UPDATE passenger SET photo='$escapedData' WHERE Name='$username'";
+                if (getimagesizefromstring($decodedData) !== false){
+                    // Escape and quote the binary data
+                    $escapedData = $conn->real_escape_string($decodedData);
 
-        if ($conn->query($sql) === TRUE) {
-            echo "Record updated successfully";
-        } else {
-            echo "Error updating record: " . $conn->error;
-        }
-        
+                    // Update user photo in the database 
+                    $sql = "UPDATE passenger SET photo=? WHERE Name=?";
+
+                    // Prepare the statement
+                    $stmt = $conn->prepare($sql);
+
+                    // Bind parameters
+                    $stmt->bind_param("ss", $decodedData, $username);
+
+                    // Execute the statement
+                    if ($stmt->execute()) {
+                        echo "Record updated successfully";
+                    } else {
+                        echo "Error updating record: " . $conn->error;
+                    }
+            
+                }
+
+            }
+
+    
 
        }
 
        if (isset($_POST["passportImage"]) ){
+
+            $data = $_POST["passportImage"];
+            $decodedData = base64_decode($data);
+
+            if($decodedData !== false){
+                if (getimagesizefromstring($decodedData) !== false){
+                    
+                    // Escape and quote the binary data
+                    $escapedData = $conn->real_escape_string($decodedData);
+
+                    // Update user photo in the database 
+                    $sql = "UPDATE passenger SET passport=? WHERE Name=?";
+
+                    // Prepare the statement
+                    $stmt = $conn->prepare($sql);
+
+                    // Bind parameters
+                    $stmt->bind_param("ss", $decodedData, $username);
+
+                    // Execute the statement
+                    if ($stmt->execute()) {
+                        echo "Record updated successfully";
+                    } else {
+                        echo "Error updating record: " . $conn->error;
+                    }
+
+                    
+
+                }
+
+            }
+
+            
+            
+
+        } 
        
-    $data = $_POST["passportImage"];
-    $decodedData = base64_decode($data);
-
-    // Escape and quote the binary data
-    $escapedData = $conn->real_escape_string($decodedData);
-
-    // Update user photo in the database directly using variable interpolation
-    $sql = "UPDATE passenger SET passport='$escapedData' WHERE Name='$username'";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Record updated successfully";
-    } else {
-        echo "Error updating record: " . $conn->error;
-    }
-} else {
-    echo "No data received.";
-       }
        
    
        mysqli_close($conn);
             
         }
+        else {
+            echo "No data received.";
+       }
 
         
 
